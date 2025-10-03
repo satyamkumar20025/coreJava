c89f229bb464cf706f560ac4615bc2798689ff93
satyamkumar20025 <satyamkumar6372@gmail.com>
2025-10-03 07:20:53 +0530

    oops

---
+++ Encapsulation_Question/Airline_reservation.java
@@ -0,0 +1,101 @@
+package Encapsulation_Question;
+
+import java.util.Scanner;
+
+public class Airline_reservation {
+
+	private String passenger_name,name;
+	private int flightno;
+	private int seatno;
+	private int person;
+	private int cancel;
+	private int totalseat=50;
+	private int tickets;
+	private double ticket_price;
+
+	void input()
+	{
+		Scanner sc = new Scanner (System.in);
+		System.out.print("Enter the passenger name:");
+		passenger_name=sc. nextLine();
+		System.out.print("Enter the flightno :");
+		flightno=sc. nextInt();
+		System.out.print("Enter the seatno :");
+		seatno=sc. nextInt();
+	}
+		
+      void calculate()
+      {
+    	  Scanner sc=new Scanner (System.in);
+    		System.out.print("Enter the ticket price :");
+    		ticket_price=sc.nextDouble();
+    		System.out.print("Enter the number of person :");
+    		person=sc.nextInt();
+    		
+                if(ticket_price>2000)
+                {
+                	ticket_price=ticket_price*person;
+            		System.out.println("ECONOMY CLASS :"+ticket_price);
+
+                }
+                else if(ticket_price>5000)
+                {
+                	ticket_price=ticket_price*person;
+            		System.out.println("BUSINESS CLASS :"+ticket_price);
+
+                }
+                else if(ticket_price>10000)
+                {
+                	ticket_price=ticket_price*person;
+            		System.out.println("FIRST CLASS :"+ticket_price);
+                }
+      		
+      }
+      void bookticket()
+      {
+        Scanner sc=new Scanner (System.in);
+  		System.out.println("WELCOME TO TICKET BOOKING SYSTEM:");
+		System.out.println("TOTAL SEAT AVAILABLE :"+totalseat);
+		System.out.println("ENTER THE YOUR NAME :");
+		name=sc.nextLine();
+		System.out.print("HOW MANY TICKETS DO YOU WANT TO BOOKING :");
+		 tickets=sc.nextInt();
+           if(tickets<=totalseat&&tickets>0)
+           {
+        	   totalseat-=tickets;
+       		System.out.print("booking successful for :"+name);
+    		System.out.print("Tickets booked :"+tickets);
+    		System.out.print("Remainig seat :"+totalseat);
+           }
+           else {
+       		System.out.print("soory booking failed not avilable :"+totalseat);
+
+           }
+
+      }
+      void iscancel()
+      {
+    	  totalseat += cancel;
+    	  tickets -= cancel;
+    	  System.out.println("TICKET CANCEL SUCCESSFUL: " + cancel);
+    	  System.out.println("Remaining seats: " + totalseat);
+      }
+      
+     void display()
+  	{
+  		System.out.println("PASSENGER NAME:"+passenger_name);
+  		System.out.println("SEAT NO :"+seatno);
+  		System.out.println("FLIGHT NO:"+flightno);
+  		System.out.println("TOTAL SEATS:"+totalseat);
+  		
+  		}
+  	
+  		public static void main(String[] args) {
+  			Airline_reservation s1=new  Airline_reservation();
+  			s1.input();
+  			s1.calculate(); 
+  			s1.bookticket();
+  			s1.iscancel();
+  			s1.display();
+  		}
+  }
---
+++ Encapsulation_Question/university_student.java
@@ -0,0 +1,100 @@
+package Encapsulation_Question;
+
+import java.util.Scanner;
+
+public class university_student {
+	String name;
+	private int roll_no;
+	private double marks1,marks2,marks3,marks4,marks5;
+	private double total,avg,per,Gpa;
+
+	 void input()
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the any student name:");
+		name=sc.nextLine();
+        System.out.print("Enter the roll no name:");
+        roll_no=sc.nextInt();
+        System.out.print("Enter the any 5 subject marks:");
+		marks1=sc.nextFloat();
+		marks2=sc.nextFloat();
+		marks3=sc.nextFloat();
+		marks4=sc.nextFloat();
+		marks5=sc.nextFloat();
+
+		}
+	void calculate()
+	{
+		
+		total=marks1+marks2+marks3+marks4+marks5;
+		avg=total/5;
+		per=total*100/500;
+	}
+	void calculate_GPA()
+	{
+		Gpa=per/9.5;
+	}
+	void ispass()
+	{
+		if(Gpa>=5.0)
+		{
+			System.out.print("pass");
+		}
+		else {
+			System.out.print("fail");
+		}
+	}
+	void Grade()
+	{
+		if(Gpa<=10&&Gpa>9)
+		{
+	      System.out.print("outstanding  A :"+Gpa);		
+		}
+		if(Gpa<=9&&Gpa>8)
+		{
+		      System.out.print("Excellent  B :"+Gpa);		
+		}
+		if(Gpa<=8&&Gpa>7)
+		{
+		      System.out.print("Good     C :"+Gpa);		
+		}
+		if(Gpa<=7&&Gpa>6)
+		{
+		      System.out.print("Average    D:"+Gpa);		
+
+		}
+		if(Gpa<=6&&Gpa>5) 
+		{
+		      System.out.print("pass   E  :"+Gpa);		
+
+		}
+	}
+	void display()
+	{
+		System.out.println("Student name:"+name);
+		System.out.println("Student rollno:"+roll_no);
+		System.out.println("Student marks1:"+marks1);
+		System.out.println("Student marks2:"+marks2);
+		System.out.println("Student marks3:"+marks3);
+		System.out.println("Student marks4:"+marks4);
+		System.out.println("Student marks5:"+marks5);
+		System.out.println("Student total marks:"+total);
+		System.out.println("Student averge marks:"+avg);
+		System.out.println("Student percenteag marks:"+per);
+		}
+	
+		public static void main(String[] args) {
+			university_student s1=new  university_student();
+			s1.input();
+			s1.calculate();
+			s1.calculate_GPA();
+			s1.Grade(); 
+			s1.display();
+		}
+}
+
+
+
+
+
+
